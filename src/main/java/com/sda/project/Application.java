f29d7eb1d8c2cfc434548b69270aeaafb0b67309
CristinaCorici <cristina.corici@gmail.com>
2019-10-14 18:48:47 +0300

    5. Configure Spring boot Thymeleaf project

--- src/main/java/com/sda/project/Application.java
+++ src/main/java/com/sda/project/Application.java
@@ -1,4 +1,4 @@
-package com.sda;
+package com.sda.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/sda/project/entities/User.java
@@ -0,0 +1,7 @@
+package com.sda.project.entities;
+
+
+public class User {
+
+
+}
