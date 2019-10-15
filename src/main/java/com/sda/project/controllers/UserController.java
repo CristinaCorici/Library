241361085672661537d6ac007969a8a87ee8e433
CristinaCorici <cristina.corici@gmail.com>
2019-10-15 19:22:14 +0300

    CRUD functionality for user

--- src/main/java/com/sda/project/controllers/UserController.java
+++ src/main/java/com/sda/project/controllers/UserController.java
@@ -5,7 +5,14 @@ import com.sda.project.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 public class UserController {
@@ -20,16 +27,67 @@ public class UserController {
     @Value("${spring.application.name}")
     String appName;
 
-    @GetMapping("/")
-    public String homePage() {
-        User user1 = new User();
-        user1.setName("Gigi");
-        user1.setEmail("gigi@gmail.com");
-        user1.setPassword("123");
-        userRepository.save(user1);
-        userRepository.findAll();
-
+//    @GetMapping("/")
+//    public String homePage() {
+//        User user1 = new User();
+//        user1.setName("Gigi");
+//        user1.setEmail("gigi@gmail.com");
+//        user1.setPassword("123");
+//        userRepository.save(user1);
+//        userRepository.findAll();
+//
 //        model.addAttribute("appName", appName);
+//        return "index";
+//    }
+
+    @GetMapping("/")
+    public String showUpdateForm(Model model) {
+        List<User> users = (List<User>) userRepository.findAll();
+        model.addAttribute("users", users);
         return "index";
     }
+
+    @GetMapping("/signup")
+    public String showSignUpForm(User user) {
+        return "register";
+    }
+
+    @PostMapping("/adduser")
+    public String addUser(@Valid User user, BindingResult result, Model model){
+        if(result.hasErrors()) {
+            return "register";
+        }
+
+        userRepository.save(user);
+        model.addAttribute("users", userRepository.findAll());
+        return "index";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String showUpdateForm(@PathVariable("id") Long id, Model model) {
+        User user = userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid user id" + id));
+        model.addAttribute("user",user);
+        return "user-edit";
+    }
+
+    @PostMapping("/update/{id}")
+    public String updateUser(@PathVariable("id") Long id, @Valid User user, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            user.setId(id);
+            return "user-edit";
+        }
+
+        userRepository.save(user);
+        model.addAttribute("users", userRepository.findAll());
+        return "index";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteUser(@PathVariable("id") Long id, Model model) {
+        User user = userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid user id" + id));
+        userRepository.delete(user);
+        model.addAttribute("users", userRepository.findAll());
+        return "index";
+    }
+
 }
--- src/main/java/com/sda/project/entities/User.java
+++ src/main/java/com/sda/project/entities/User.java
@@ -34,6 +34,14 @@ public class User {
         return name;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public void setName(String name) {
         this.name = name;
     }
